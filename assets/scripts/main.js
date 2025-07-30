aca985336633cf68dbbe1669a0051179a2135efb
Victor Balena <victorbalenaes@gmail.com>
2025-07-30 11:13:53 -0300

    Finished Footer and Started Media Queries

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -79,6 +79,29 @@ function activeProducts() {
 linkProducts.forEach(l=>l.addEventListener('click', activeProducts))
 
 /*====== SHOW SCROLL UP ======*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  //Quando o scroll for maior que 350vh, adiciona a classe show-scroll à tag com  a classse scroll-top
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUp)
 
 /*====== SCROLL SECTIONS ACTIVE LINK ======*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollAtive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+     sectionTop = current.offsetTop - 58,
+     sectionId = current.getAttribute('id')
+
+     if(scrollY > sectionTop && scrollY <= scetionTop + sectionHeight)
+      document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.add('active-link')
+    else
+      document.querySelector(`.nav__menu a[href*='${sectionId}']`).classList.remove('active-link')
+    })
+}
